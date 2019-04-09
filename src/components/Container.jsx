14272932871b9361e21bf13ce2dbfccdd3ebb906
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-04-09 17:31:26 +0200

    initial

---
+++ src/components/Container.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Container.less';
+
+export default class Container extends Component {
+  render() {
+    return (
+      <div className='container-hello-world'>
+        Hello, World!
+      </div>
+    );
+  }
+}
